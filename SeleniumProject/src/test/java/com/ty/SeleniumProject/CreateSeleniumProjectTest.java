58dc324c52712ce656f22a18887f51dea32cb032
rohit0596 <rohit.raj.0696@gmail.com>
2021-08-04 20:14:56 +0530

    This is Evening commit

---
+++ SeleniumProject/src/test/java/com/ty/SeleniumProject/CreateSeleniumProjectTest.java
@@ -0,0 +1,11 @@
+package com.ty.SeleniumProject;
+
+import org.testng.annotations.Test;
+
+public class CreateSeleniumProjectTest {
+
+	@Test
+	public static void createSeleniumProject() {
+		System.out.println("hii...this method do selenium create PROJECT");
+	}
+}
